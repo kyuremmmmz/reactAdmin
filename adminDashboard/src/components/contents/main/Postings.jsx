2d8044ace00db7e208d031f19ca29cb0d8ce2312
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-24 23:12:46 +0800

    done css

--- adminDashboard/src/components/contents/main/Postings.jsx
+++ adminDashboard/src/components/contents/main/Postings.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../panels/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Card, Container, ListGroup } from 'react-bootstrap';
+import { Button, Card, Container, ListGroup, Row, Col } from 'react-bootstrap';
 import { supabase } from '../../../supabaseClient';
 import ModalWidget from './modal/ModalWidget';
 import InsertionModalWidget from './modal/InsertionModalWidget';
@@ -55,40 +55,27 @@ function Postings() {
       <Header />
       <main className='main'>
         <div className="container mt-5">
-          <h2 className="text-center">Postings</h2>
-          <Button variant='success' className='mt-4' onClick={handleEditClick}>Add Hotel</Button>
+          <Row>
+            <h2 className="text-center w-25 col-1">Hotel Postings</h2>
+            <Button variant='success' className=' col-2' onClick={handleEditClick}>Add Hotel</Button>
+          </Row>
           <ListGroup className="mt-4">
             {dataFetched.map((hotel) => (
-              <ListGroup.Item key={hotel.id} className="mb-4">
-                <Card style={{ width: '100%', height: 'auto', display: 'flex', flexDirection: 'row', border: '1px solid #007bff', borderRadius: '0.5rem' }} className="shadow-sm">
-                  <Card.Body style={{ flex: 1 }}>
-                    <Card.Title className="text-center">{hotel.hotel_name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted text-center">
-                      Price: PHP {hotel.hotel_price}
-                    </Card.Subtitle>
-                    <Card.Text>
-                      <strong>Amenities:</strong>
-                      <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
-                        {[...Array(20).keys()].map((index) => {
-                          const amenityKey = `amenity${index + 1}`;
-                          return hotel[amenityKey] && <li key={index}>{hotel[amenityKey]}</li>;
-                        })}
-                      </ul>
-                    </Card.Text>
-                    <Card.Text>
-                      <strong>Description:</strong> {hotel.hotel_description}
-                    </Card.Text>
-                    <div className="text-center">
-                      <Button variant="primary" onClick={() => handleEditClickModal(hotel)} className="mx-2">
-                        Edit
-                      </Button>
-                      <Button variant="danger" className="mt-2" onClick={() => deleteData(hotel.id)}>
-                        Delete
-                      </Button>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </ListGroup.Item>
+              <Container key={hotel.id} className='color'>
+                <Row className='object-fit-cover'>
+                  <div className='width'>
+                    <img  className='pic' src={`https://tglolshdsrixggmpvujc.supabase.co/storage/v1/object/public/hotel_amenities_url/${hotel.image}`} />
+                  </div>
+                </Row>
+                <div className="text-center">
+                  <Button variant="primary" onClick={() => handleEditClickModal(hotel)} className="mx-2">
+                    Edit
+                  </Button>
+                  <Button variant="danger" onClick={() => deleteData(hotel.id)}>
+                    Delete
+                  </Button>
+                </div>
+              </Container>
             ))}
           </ListGroup>
 
