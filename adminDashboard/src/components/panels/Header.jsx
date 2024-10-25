934fe3ac2a15ef75ff3211d61fb79aacb0d7ab44
unknown <peterpagasartonga@gmail.com>
2024-10-25 15:22:59 +0800

    Added titlelogo

--- adminDashboard/src/components/panels/Header.jsx
+++ adminDashboard/src/components/panels/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import titleIcon from '../../assets/titlelogo.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <h2 className="header-title">TRAVEL GO</h2>
-      <p className="header-subtitle">Northwestern part of Luzon Island, Philippines</p>
+      <img
+        src={titleIcon}
+        alt="profile"
+        height={180}
+        width={500}
+      />
     </header>
   );
 };
