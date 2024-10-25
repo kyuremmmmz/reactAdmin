e2d6108d4898660ab1c5b24f4fcd4786715fa3b6
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-25 13:07:36 +0800

    added alr

--- adminDashboard/src/components/contents/main/Transactions.jsx
+++ adminDashboard/src/components/contents/main/Transactions.jsx
@@ -1,16 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../panels/Header'
 import { Container, Tabs, Tab, Table } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { supabase } from '../../../supabaseClient';
+import Swal from 'sweetalert2';
 function Transactions() {
+    const [data, setData] = useState([]);
 
+    const fethData = async () => {
+        try {
+            const { data, error } = await supabase.from('payment_table').select('*');
+            if (error) throw error;
+            setData(data);
+        } catch (e) {
+            console.error(e);
+            Swal.fire('The Internet?', 'That thing is still around?', 'question');
+        }
+    }
+
+    useEffect(() => {
+        fethData();
+    }, [])
   return (
     <div>
         <Header />
         <Container>
             <h2>Transactions</h2>
-            <Table striped hover bordered>
+            <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th className=' text-center'>Name</th>
@@ -19,15 +36,25 @@ function Transactions() {
                         <th className=' text-center'>Booking ID</th>
                         <th className=' text-center'>Phone</th>
                         <th className=' text-center'>Paid Via</th>
+                        <th className=' text-center'>Payment Amount</th>
+                        <th className=' text-center'>Date of Payment</th>
                     </tr>
-                        <tr className=' text-center'>
-                            <td>John Doe</td>
-                            <td>johndoe@example.com</td>
-                            <td>123456789</td>
-                            <td>12345</td>
-                            <td>1234567890</td>
-                            <td>John Doe</td>
-                        </tr>
+                    {data.map((item) => (
+                            <tr key={item.id} className=' text-center'>
+                            <td>{ item.name }</td>
+                            <td>{ item.gmail }</td>
+                            <td>{ item.reference_number }</td>
+                            <td>{ item.booking_id }</td>
+                            <td>+630{ item.phone }</td>
+                                <td>
+                                <div className='payment-card'>
+                                    <img src={item.pay_via === 'paypal'? 'https://img.shields.io/badge/PayPal-00457C?style=for-the-badge&logo=paypal&logoColor=white' : 'https://img.shields.io/badge/Credit%20Card-blue?style=for-the-badge&logo=stripe&logoColor=white'} alt='Payment Type' />
+                                    </div>
+                            </td>
+                            <td>{item.payment}</td>
+                            <td>{item.date_of_payment}</td>
+                            </tr>
+                    ))}
                 </thead>
             </Table>
         </Container>
