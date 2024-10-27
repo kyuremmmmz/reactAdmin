8b602c02e2b7d515042d63cd7e1659870b6f7ee2
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-10-27 17:07:39 +0800

    added

--- adminDashboard/src/components/contents/main/Flights.jsx
+++ adminDashboard/src/components/contents/main/Flights.jsx
@@ -1,7 +1,25 @@
 import react from "react";
+import Header from "../../panels/Header";
+import { Button, Col, Row } from "react-bootstrap";
 
 const Flights = () => {
-  return <div>Flights</div>;
+  return (
+    <div>
+      <Header />
+      <main className="main">
+        <Row>
+          <div className=" m-4 col-1">
+            <h2>Flights</h2>
+          </div>
+          <div className=" m-4 col-3">
+            <Button variant="success">
+                Add Flights
+            </Button>
+          </div>
+        </Row>
+      </main>
+    </div>
+  );
 };
 
 export default Flights;
